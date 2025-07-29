74ccd39890e9bae93e244a04d958f2e3b2228b36
Adrian Lyjak <adrianlyjak@gmail.com>
2025-07-29 18:29:51 -0400

    adding dependencies

--- test-proj/ui/src/components/workflow-trigger.tsx
+++ test-proj/ui/src/components/workflow-trigger.tsx
@@ -55,12 +55,13 @@ export default function TriggerFileWorkflow({
   useEffect(() => {
     const lastEvent = wf.events[wf.events.length - 1];
     if (lastEvent?.type.endsWith("process_file.UIToast")) {
-      if (lastEvent.data.level === "info") {
-        toast.info(lastEvent.data.message);
-      } else if (lastEvent.data.level === "warning") {
-        toast.warning(lastEvent.data.message);
-      } else if (lastEvent.data.level === "error") {
-        toast.error(lastEvent.data.message);
+      const event = lastEvent as UIToast;
+      if (event.data.level === "info") {
+        toast.info(event.data.message);
+      } else if (event.data.level === "warning") {
+        toast.warning(event.data.message);
+      } else if (event.data.level === "error") {
+        toast.error(event.data.message);
       }
     }
   }, [wf.events.length]);
