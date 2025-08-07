a46c002255ff924faf1ac8215524a11141e74563
Adrian Lyjak <adrianlyjak@gmail.com>
2025-08-07 19:47:05 -0400

    feat: support scoping by project id (#11)

--- test-proj/ui/src/lib/client.ts
+++ test-proj/ui/src/lib/client.ts
@@ -11,12 +11,17 @@ import { EXTRACTED_DATA_COLLECTION } from "./config";
 
 const platformToken = import.meta.env.VITE_LLAMA_CLOUD_API_KEY;
 const apiBaseUrl = import.meta.env.VITE_LLAMA_CLOUD_BASE_URL;
+const projectId = import.meta.env.VITE_LLAMA_CLOUD_PROJECT_ID;
 
 // Configure the platform client
 cloudApiClient.setConfig({
   baseUrl: apiBaseUrl,
   headers: {
+    // optionally use a backend API token scoped to a project. For local development,
     ...(platformToken && { authorization: `Bearer ${platformToken}` }),
+    // This header is required for requests to correctly scope to the agent's project
+    // when authenticating with a user cookie
+    ...(projectId && { "Project-Id": projectId }),
   },
 });
 
