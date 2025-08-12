6117cd8922e33f5595e04c1672300a6e820f26a6
Adrian Lyjak <adrianlyjak@gmail.com>
2025-08-12 11:22:03 -0400

    llamactl updates (#12)
    * fix: update for new llamactl compatibility
    
    * Fixing template

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,17 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "@llamaindex/ui/styles.css";
 import "./index.css";
 
-// https://github.com/run-llama/llama_deploy/blob/main/llama_deploy/apiserver/deployment.py#L183
-const base = import.meta.env.VITE_LLAMA_DEPLOY_BASE_PATH ?? "/";
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename={base}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
